357df06866e5a2da75d4232940b491a8b953ba09
Unknown <hita0718-yuki10maga-kira.6978@i.softbank.jp>
2018-05-21 17:09:53 +0900

    差所のコミット

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;//回転速度
+    private void Start()
+    {
+        
+    }
+    void Update () {
+        //マウスが押されたら回転速度設定する
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+
+        }
+        //回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //ルーレットを原則させる（追加）
+        this.rotSpeed *= 0.99f;
+
+    }
+}
